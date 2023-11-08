754948144711d8f230c3d871d872b289edc4aee5
oyunojino <dusrn2xlayjoh@gmail.com>
2023-11-09 00:50:54 +0900

    first commit

---
+++ src/main/java/com/example/jpaorder/JpaOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOrderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JpaOrderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/jpaorder/JpaOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaOrderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
