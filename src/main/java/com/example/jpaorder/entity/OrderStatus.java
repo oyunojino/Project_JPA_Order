f0a665e45e4e0d8d77c6d666df9e48b0e221a545
oyunojino <dusrn2xlayjoh@gmail.com>
2023-11-09 01:32:39 +0900

    주문 상태, 회원 타입 enum 추가

---
+++ src/main/java/com/example/jpaorder/entity/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.example.jpaorder.entity;
+
+public enum OrderStatus {
+    CREATED, TOBEDELIVERED, PAYED, COMPLETED
+}
---
+++ src/main/java/com/example/jpaorder/entity/UserType.java
@@ -0,0 +1,5 @@
+package com.example.jpaorder.entity;
+
+public enum UserType {
+    BASIC, VIP
+}
