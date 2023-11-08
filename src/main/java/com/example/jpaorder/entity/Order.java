553460c59eb22a0ab48acdc914c7714374bd8ca3
oyunojino <dusrn2xlayjoh@gmail.com>
2023-11-09 01:30:50 +0900

    주문 엔티티 추가

---
+++ src/main/java/com/example/jpaorder/entity/Order.java
@@ -0,0 +1,30 @@
+package com.example.jpaorder.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // 지연로딩
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name="order_date", length = 10)
+    private String orderDate;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name="order_status", length = 10)
+    private OrderStatus orderStatus;
+
+    private long totalPrice;
+    private int totalQuantity;
+}
