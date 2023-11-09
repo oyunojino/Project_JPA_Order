4c44cb59e13cde1103e795cae71614790b304293
oyunojino <dusrn2xlayjoh@gmail.com>
2023-11-09 16:08:27 +0900

    상품 관련 서비스 update 에러 수정

--- src/main/java/com/example/jpaorder/service/ProductService.java
+++ src/main/java/com/example/jpaorder/service/ProductService.java
@@ -33,10 +33,13 @@ public class ProductService {
     productRepository.deleteById(productId);
   }
 
+//  ERROR ) Optional 타입을 Product 타입으로 변경 하기 위해 .get() 추가
   public void updateProduct(ProductUpdateDto productUpdateDto) {
+//    # 재고 수량은 수정불가하기 때문에 ProductUpdateDto 사용
     Product product = productRepository.findById(productUpdateDto.getProductId()).get();
     product.setName(productUpdateDto.getName());
     product.setPrice(productUpdateDto.getPrice());
+//    product.setQuantity(productUpdateDto.getQuantity());
     productRepository.save(product);
   }
 
